29f34ea0e901dccf735ffaa27a32a460686d1a29
ts moosavi <tt.musavi@gmail.com>
2022-04-11 04:24:37 +0430

    an instance of app database Created and Dao methods used

--- app/src/main/java/com/example/livedata/QuestionRepository.kt
+++ app/src/main/java/com/example/livedata/QuestionRepository.kt
@@ -1,10 +1,30 @@
 package com.example.livedata1
 
+import android.content.Context
+import com.example.livedata.DaoOfQuestion
+import com.example.livedata.MyDatabase
+import com.example.livedata.QuestionEntity
+
 object QuestionRepository {
-    val questionList = arrayListOf<Question>(
-       Question( " result of 2 + 2 ",4) ,
-       Question(" result of  5 - 1 " , 4) ,
-        Question(" result of 100 * 21",2100)
-    )
+    var db: MyDatabase? = null
+    var questionDao: DaoOfQuestion? = null
+    fun initDB(context:Context ){
+        db = MyDatabase.getAppDataBase(context)
+        questionDao = db?.daoOfQuestion()
+         addTestData()
+    }
+
+   fun addTestData() {
+        questionDao?.insertAll(
+            QuestionEntity( 1," result of 2 + 2 ",4) ,
+            QuestionEntity(2," result of  5 - 1 " , 4) ,
+            QuestionEntity(3," result of 100 * 21",2100))
+    }
+
+//    val questionList = arrayListOf<Question>(
+//       Question( " result of 2 + 2 ",4) ,
+//       Question(" result of  5 - 1 " , 4) ,
+//        Question(" result of 100 * 21",2100)
+//    )
 }
-class Question(var question:String,var  answer: Int)
+//class Question(var question:String,var  answer: Int)
