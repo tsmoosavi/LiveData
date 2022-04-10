7eeca1a7c2e3a0f4aabd820ba85c903b04b961e3
ts moosavi <tt.musavi@gmail.com>
2022-04-11 02:18:42 +0430

    a class added to QuestionRepository.kt

--- app/src/main/java/com/example/livedata/QuestionRepository.kt
+++ app/src/main/java/com/example/livedata/QuestionRepository.kt
@@ -1,9 +1,10 @@
 package com.example.livedata1
 
 object QuestionRepository {
-    val questionList = arrayListOf(
-        " result of 2 + 2 " ,
-        " result of  5 - 1 " ,
-        " result of 100 * 21"
+    val questionList = arrayListOf<Question>(
+       Question( " result of 2 + 2 ",4) ,
+       Question(" result of  5 - 1 " , 4) ,
+        Question(" result of 100 * 21",2100)
     )
 }
+class Question(var question:String,var  answer: Int)
