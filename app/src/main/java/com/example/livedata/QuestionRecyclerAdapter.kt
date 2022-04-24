419e2d6c7f39e0faa40767baeb8b6ac5c0b841c6
ts moosavi <tt.musavi@gmail.com>
2022-04-24 16:07:56 +0430

    dataBinding added to recyclerview

--- app/src/main/java/com/example/livedata/QuestionRecyclerAdapter.kt
+++ app/src/main/java/com/example/livedata/QuestionRecyclerAdapter.kt
@@ -5,28 +5,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.livedata.databinding.QuestionRecyclerviewBinding
 
 class QuestionRecyclerAdapter:
     ListAdapter<QuestionEntity, QuestionRecyclerAdapter.ItemHolder>(QuestionDiffCallback) {
-        class ItemHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var questionText = view.findViewById<TextView>(R.id.questionText)
-        var questionId = view.findViewById<TextView>(R.id.questionId)
-        }
+        class ItemHolder(val binding: QuestionRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ItemHolder {
+        val binding: QuestionRecyclerviewBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.question_recyclerview,
+            parent,false
+        )
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.question_recyclerview, parent, false)
 
-        return ItemHolder(view)
+        return ItemHolder(binding)
     }
 
     override fun onBindViewHolder(holder:ItemHolder , position: Int) {
-        holder.questionText.text = getItem(position).questionText
-        holder.questionId.text = getItem(position).number.toString()
+      holder.binding.question = getItem(position)
     }
 
     object QuestionDiffCallback : DiffUtil.ItemCallback<QuestionEntity>() {
